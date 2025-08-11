2660f8af5adb8f88ecc632cf2841ed0d1b4433e8
PatrickPonce <patrick_ponce1@usmp.pe>
2025-08-10 21:57:13 -0500

    Mejoras

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -11,10 +11,10 @@ namespace programacion1_tarea3.Controllers
         {
             return new List<Producto>
             {
-                new Producto { Nombre = "Collar para perro", Precio = 25.50m },
-                new Producto { Nombre = "Juguete para gato", Precio = 15.00m },
-                new Producto { Nombre = "Cama para mascota", Precio = 80.00m },
-                new Producto { Nombre = "Comida para perro 5kg", Precio = 120.00m }
+                new Producto { Nombre = "Collar para perro", Precio = 25.50m, ImagenUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbJegywO2EXTj-C1c_NzSpAJPTLTt5CEMD2A&s" },
+                new Producto { Nombre = "Juguete para gato", Precio = 15.00m, ImagenUrl = "https://guaw.com/30188-home_default/juguete-surtido-ferplast-5017-para-gato.jpg" },
+                new Producto { Nombre = "Cama para mascota", Precio = 80.00m, ImagenUrl = "https://www.clubdeperrosygatos.cl/wp-content/uploads/2024/05/cama-ortopedica.webp" },
+                new Producto { Nombre = "Comida para perro 5kg", Precio = 120.00m, ImagenUrl = "https://www.acerix.com.uy/imgs/productos/productos35_55040.jpg" }
             };
         }
 
--- Models/Producto.cs
+++ Models/Producto.cs
@@ -5,5 +5,6 @@ namespace programacion1_tarea3.Models
         public string Nombre { get; set; }
         public decimal Precio { get; set; }
         public bool Seleccionado { get; set; }
+        public string ImagenUrl { get; set; }
     }
 }
